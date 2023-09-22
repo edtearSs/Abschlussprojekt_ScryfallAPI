5da9e3638e420782cd618757cd48b36a73ae8401
edtearSs <eric.dahlmann@yahoo.de>
2023-09-22 14:31:53 +0200

    added analyze function using vectoring and naive_bayes, added correct filtering for the dataframe

--- naive_bayes_machine_learning.py
+++ naive_bayes_machine_learning.py
@@ -27,6 +27,53 @@ def get_dataframe():
     # Create a DataFrame from the list of rows
     df = pd.DataFrame(rows, columns=["Card Name", "Mana Cost", "Oracle Text", "Colors", "Color Identity", "Rarity", "Type Line"])
 
+    filtered_df = df[~df['Type Line'].apply(lambda x: 'token' in x.lower()
+                                                      or 'card' in x.lower() or 'scheme' in x.lower()
+                                                      or 'vanguard' in x.lower() or 'emblem' in x.lower()
+                                                      or 'hero' in x.lower() or 'conspiracy' in x.lower()
+                                                      or 'phenomenon' in x.lower() or 'stickers' in x.lower()
+                                                      or 'summon' in x.lower() or 'tolkien' in x.lower()
+                                                      or 'plane — ' in x.lower())]
+
+    # Reset the index of the filtered DataFrame
+    filtered_df.reset_index(drop=True, inplace=True)
+
+    # Create a copy of the filtered DataFrame before dropping rows
+    filtered_df_copy = filtered_df.copy()
+
+    # Drop rows with missing values in specified columns
+    df = filtered_df_copy.dropna(axis='index', subset=['Oracle Text'])
+
     # Print the DataFrame
-    #print(df)
     return df
+
+def analyze(keywords):
+    df = get_dataframe()
+
+    vect = CountVectorizer()
+    wordsCountArray = vect.fit_transform(df['Oracle Text'])
+
+    X_train, X_test, y_train, y_test = train_test_split(wordsCountArray, df['Colors'], test_size=0.2, random_state=0)
+
+    model = MultinomialNB()
+    model.fit(X_train, y_train)
+
+    # Calculate the likelihood of each color for the given keywords
+    keyword_text = " ".join(keywords)
+    keyword_vector = vect.transform([keyword_text])
+
+    likelihood = model.predict_proba(keyword_vector)
+    colors = model.classes_
+
+    s = f"Likelihood of Colors for Keywords: {', '.join(keywords)}\n\n"
+
+    for color, prob in zip(colors, likelihood[0]):
+        s += f"{color}: {prob * 100:.2f}%\n"
+
+    st.markdown(s)
+
+    return model, vect
+
+# Example usage:
+#selected_keywords = ["power", "toughness"]  # Replace with your chosen keywords
+#model, vect = analyze(selected_keywords)
