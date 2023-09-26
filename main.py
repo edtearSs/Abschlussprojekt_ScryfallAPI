3bae9e67b5105125be2b6eef87cdb5fb7e7ca519
edtearSs <eric.dahlmann@yahoo.de>
2023-09-26 17:18:26 +0200

    added date_inputs in sidebar. added second column to be able to compare data. changed the class names.

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
+import datetime
+
 import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import datetime
 from scryfallAPI import fetch_card, MaximumRequestDone, WrongCardName
 from naive_bayes_machine_learning import get_dataframe, analyze
 
 st.set_page_config(layout="wide")
 sns.set_theme()
 
-col1, col2 = st.columns(2)
+col1, col2 = st.columns([1, 1])
 
 if "vect" not in st.session_state:
     # Der Vectorizer für unser Model
@@ -24,8 +27,11 @@ if "data" not in st.session_state:
     # Hier muss der Dataframe in den session state aufgenommen werden
     st.session_state.data = get_dataframe()
 
-name = st.sidebar.text_input("Kartenname", placeholder="Schreib hier einen Kartennamen.")
-print(name)
+date1 = st.sidebar.date_input("Choose the Release Dates for the Data you want to use.", value=datetime.datetime.now(),
+                              min_value=datetime.date(1993, 11, 1))
+date2 = st.sidebar.date_input("Choose a second Date to compare the Data.", value=datetime.datetime.now(),
+                              min_value=datetime.date(1993, 11, 1))
+name = st.sidebar.text_input("Card Name", placeholder="Write a Card Name.")
 
 if name:
     try:
@@ -38,17 +44,41 @@ if name:
     except Exception as e:
         st.error(f"An error occurred: {str(e)}")
 
-with col1:
+btn1 = st.button("Press this button to show the entire Dataframe")
+if btn1:
+    st.dataframe(st.session_state.data, width=1600)
 
-    st.dataframe(st.session_state.data, width=800)
+class_name_mapping = {'': 'Colorless',
+                      'B': 'Black',
+                      'G': 'Green',
+                      'R': 'Red',
+                      'U': 'Blue',
+                      'W': 'White'}
+
+with col1:
+    st.session_state.model, st.session_state.vect = analyze()
+
+    text1 = col1.text_input("Which color is this text most likely to be part of?", key="t1",
+                          value="Destroy target Creature")
+
+    if st.session_state.model != None and st.session_state.vect != None:
+        propas = st.session_state.model.predict_proba(st.session_state.vect.transform([text1]))
+
+        for i in range(len(st.session_state.model.classes_)):
+            original_class_name = st.session_state.model.classes_[i]
+            new_class_name = class_name_mapping.get(original_class_name, original_class_name)
+            st.markdown(f"**{new_class_name}**: {propas[0][i] * 100:.2f} % ")
 
 with col2:
     st.session_state.model, st.session_state.vect = analyze()
 
-    text = st.text_input("Which color is this text most likely to be part of?")
+    text2 = col2.text_input("Which color is this text most likely to be part of?", key="t2",
+                          value="Draw a Card")
 
     if st.session_state.model != None and st.session_state.vect != None:
-        propas = st.session_state.model.predict_proba(st.session_state.vect.transform([text]))
+        propas = st.session_state.model.predict_proba(st.session_state.vect.transform([text2]))
 
         for i in range(len(st.session_state.model.classes_)):
-            st.markdown(f"**{st.session_state.model.classes_[i]}**: {propas[0][i]*100:.2f} % ")
+            original_class_name = st.session_state.model.classes_[i]
+            new_class_name = class_name_mapping.get(original_class_name, original_class_name)
+            st.markdown(f"**{new_class_name}**: {propas[0][i] * 100:.2f} % ")
